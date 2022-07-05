791250fad16b27a10d30055524ac4ebf3d44e6af
Sergei <serhio1@inbox.ru>
2022-07-06 00:09:11 +0500

    Slider adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,9 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
+        adaptiveWidth: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
     });
     
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
